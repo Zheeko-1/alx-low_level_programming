189281ed57aa49da5f7cc431b5d0653af4f6cbb4
Zheeko-1 <orok.isaac@gmail.com>
2022-12-08 22:09:16 +0000

    6 fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,3 +17,4 @@ int main(void)
 	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
+}
