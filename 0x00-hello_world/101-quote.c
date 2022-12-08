4c88e93d34a6b4b37d8aa7e5428f0a1e6c229583
Zheeko-1 <orok.isaac@gmail.com>
2022-12-08 21:03:38 +0000

    task 6 and 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
  *main - prints a program that returns 1
  *
  *Return: 1
 */
 int main(void)
 {
-	putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
