503c2eeee12b0b7a0d0287db9c7cf8455c9add6d
Zheeko-1 <orok.isaac@gmail.com>
2022-12-08 20:06:31 +0000

    task 1-7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *main - prints a program that returns 1
+ *
+ *Return: 1
+*/
+int main(void)
+{
+	putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return (1);
+}
