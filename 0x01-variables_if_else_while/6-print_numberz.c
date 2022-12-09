6cca58868edf13e89286c5da9b109954c36439af
Zheeko-1 <orok.isaac@gmail.com>
2022-12-09 23:03:34 +0000

    using putchar to print 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar((a % 10) + '0');
+	putchar('\n');
+	return (0);
+}
