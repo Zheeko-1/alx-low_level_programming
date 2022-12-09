3945fe25bbaa6cf42218460fd87acf57360d4001
Zheeko-1 <orok.isaac@gmail.com>
2022-12-09 23:30:19 +0000

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry
+ * Return: 0
+*/
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	for (b = 'a'; b < 'f'; a++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
