76f442984694cf5ea267b52ab2da674f7e5bb8a8
Zheeko-1 <orok.isaac@gmail.com>
2022-12-09 21:36:18 +0000

    do not print q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a'
+	for (n = 'a'; n <= 'z' && n != e  && n != q; n++)
+	{
+		putchar(n);
+	}
+	putchar(\n);
+
+}
