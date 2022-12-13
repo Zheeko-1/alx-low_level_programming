65582972b35d077c8b6df9a660a22bce4ae933b6
Zheeko-1 <orok.isaac@gmail.com>
2022-12-13 11:09:21 +0000

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  *
  *Return - 0
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10()
 {
 	int a;
 	char alp;
@@ -17,5 +17,5 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
-	return (0);
+
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *_islower - Entry
+ *
+ *Return: 0
+ */
+int _islower(int c)
+{
+	
