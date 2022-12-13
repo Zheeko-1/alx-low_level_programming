f7c34196bff26c083267ce32f5a6d6291d1c604e
Zheeko-1 <orok.isaac@gmail.com>
2022-12-13 12:22:16 +0000

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,13 @@
 #include "main.h"
 /**
  *_islower - Entry
- *
- *Return: 0
+ *@c: character
+ *Return: 1 or 0
  */
 int _islower(int c)
 {
-	
+	if ((c >= 'a' && c <= 'z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
