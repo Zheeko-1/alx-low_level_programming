8153e54ab2a493800632e9302f4f35eee51bebdf
Zheeko-1 <orok.isaac@gmail.com>
2022-12-14 07:36:57 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,15 +4,16 @@
  *print_to_98 - Entry
  *
  *@n: character
+ *Returm: 0 or 1
  */
 void print_to_98(int n)
 {
-	while (n > 98)
+	while (n < 98)
 	{
 		printf("%d, ", n);
 		n++;
 	}
-	while (n < 98)
+	while (n > 98)
 	{
 		printf("%d, ", n);
 		n--;
