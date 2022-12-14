203bbd8df198a018251d18b5029b9114d318506b
Zheeko-1 <orok.isaac@gmail.com>
2022-12-14 07:16:39 +0000

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -25,14 +25,14 @@ int main(void)
 	}
 
 	bef1 = (bef / l;
-	bef1 = (bef % l);
-	aft1 = (bef / l);
-	aft2 = (bef % l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
 
 	for (i = 92; 1 < 99; ++i)
 	{
 		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l;
+		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
