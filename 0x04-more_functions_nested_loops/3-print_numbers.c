b18d5113da5fa7b1693ce86594931628475898b3
Zheeko-1 <orok.isaac@gmail.com>
2022-12-16 00:15:28 +0000

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point of code
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (i = 0; 1 <= 9; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
