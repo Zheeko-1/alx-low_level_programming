9e57aba3b7afd9ee0bb1eb7c79d07552cbe4a63d
Zheeko-1 <orok.isaac@gmail.com>
2022-12-16 00:58:09 +0000

    main fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,16 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+int _putchar(char);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -13,4 +20,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
+#endif /* MAIN_H */
