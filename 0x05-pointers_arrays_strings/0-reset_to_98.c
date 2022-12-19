5c4d03d33d77f516dfb18208869d5dae87c688fe
Zheeko-1 <orok.isaac@gmail.com>
2022-12-19 18:10:00 +0000

    all task for pointer and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * reset_to_98-Resets the value of a variable back to 98.
+ * @n:Variable to be reset.
+ * Return:Void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+Task 1
+#include "main.h"
+
+/**
+ * swap_int-Swaps the values of 2 integers.
+ * @a:First argument to be swapped.
+ * @b:second argument to be swapped.
+ *
+ * Return:void.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int p = *a;
+	*a = *b;
+	*b = p;
+
+}
