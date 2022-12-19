f2e7b5580d9f3c1c6c08c6df4982175a6c542098
Zheeko-1 <orok.isaac@gmail.com>
2022-12-19 18:14:59 +0000

    fix 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,30 +1,11 @@
 #include "main.h"
-/**
- * reset_to_98-Resets the value of a variable back to 98.
- * @n:Variable to be reset.
- * Return:Void.
- */
 
+/**
+ *  * reset_to_98 - resets value to 98
+ *   *
+ *    * @n: pointer integer
+ *     */
 void reset_to_98(int *n)
 {
-	*n = 98;
-}
-
-Task 1
-#include "main.h"
-
-/**
- * swap_int-Swaps the values of 2 integers.
- * @a:First argument to be swapped.
- * @b:second argument to be swapped.
- *
- * Return:void.
- */
-
-void swap_int(int *a, int *b)
-{
-	int p = *a;
-	*a = *b;
-	*b = p;
-
+		*n = 98;
 }
